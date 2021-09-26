b7553952c3780ded98d3d7cfb6922818f710b7a3
Haccess <haccesssmc@gmail.com>
2021-09-26 04:24:23 +0300

    Main Menu Implementation
    + Main functionality of the Menu has implementated
    + Save/Load functional compleat

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,157 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+        using UnityEditor;
+#endif
+
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public string userName;
+    public int result;
+
+    private SaveData data = new SaveData();
+
     private void Awake()
     {
-        if(Instance == null)
+        if(Instance != null)
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        Load();
+
+        SetActiveUser(data.currentUserID);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Exit()
+    {
+        Save();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    [Serializable]
+    public class SaveData
+    {
+        public List<string> names = new List<string>();
+        public List<int> results = new List<int>();
+        public int currentUserID = -1;
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(data);
+        string path = Application.dataPath + "/save.json";
+        File.WriteAllText(path, json);
+    }
+
+    public void Load()
+    {
+        string path = Application.dataPath + "/save.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            data = JsonUtility.FromJson<SaveData>(json);
+        }
+    }
+
+    public List<string> GetUsersNames()
+    {
+        return data.names;
+    }
+
+    public List<int> GetUsersResults()
+    {
+        return data.results;
+    }
+
+    public void AddNewUser(string name)
+    {
+        if (name.Length <= 0 || data.names.Exists(x => x.StartsWith(name))) return;
+
+        data.names.Add(name);
+        data.results.Add(0);
+        SetActiveUser(data.names.Count - 1);
+    }
+
+    public void SetActiveUser(int id)
+    {
+        data.currentUserID = id;
+
+        if(id > -1)
+        {
+            userName = data.names[id];
+            result = data.results[id];
         }
         else
         {
-            Destroy(gameObject);
+            userName = "";
+            result = 0;
         }
+    }
+
+    public void DeleteUser(int id)
+    {
+        if(data.currentUserID == id)
+        {
+            SetActiveUser(id - 1);
+        }
+
+        data.names.RemoveAt(id);
+        data.results.RemoveAt(id);
+
+    }
+
+    public List<string> GetTopResults()
+    {
+        List<string> top = new List<string>();
+        List<string> names = data.names;
+        List<int> results = data.results;
+
+        while(top.Count < 3 && results.Count > 0)
+        {
+            int maxResult = 0;
+            int id = -1;
+            for(int i = 0; i < results.Count; i++)
+            {
+                if(results[i] > maxResult)
+                {
+                    id = i;
+                    maxResult = results[id];
+                }
+                else if(results[i] == 0)
+                {
+                    results.RemoveAt(i);
+                    names.RemoveAt(i);
+                }
+            }
+            if(id > -1)
+            {
+                top.Add(name[id] + " : " + results[id]);
+                results.RemoveAt(id);
+                names.RemoveAt(id);
+            }
+        }
+
+        return top;
     }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI helloText;
+    [SerializeField] TextMeshProUGUI resultText;
+    [SerializeField] GameObject addNewUserObj;
+    [SerializeField] List<GameObject> usersInfo;
+    [SerializeField] List<GameObject> topInfo;
+    
+
+    private void Start()
+    {
+        onLoadMainScreen();
+    }
+
+    public void onLoadMainScreen()
+    {
+        if (GameManager.Instance != null)
+        {
+            if (GameManager.Instance.userName != "")
+            {
+                helloText.text = "Hello, " + GameManager.Instance.userName + "!";
+            }
+            else
+            {
+                helloText.text = "Hello, Gamer!";
+            }
+
+            if (GameManager.Instance.result > 0)
+            {
+                resultText.text = "Your top result is " + GameManager.Instance.result;
+            }
+            else
+            {
+                resultText.text = "Your top result is 0";
+            }
+        }
+    }
+
+    public void onLoadUsersScreen()
+    {
+        List<string> userNames = GameManager.Instance.GetUsersNames();
+        List<int> userResults = GameManager.Instance.GetUsersResults();
+
+        int count = userNames.Count;
+        int maxCount = usersInfo.Count;
+        if(count < maxCount)
+        {
+            addNewUserObj.SetActive(true);
+        }
+        else
+        {
+            addNewUserObj.SetActive(false);
+        }
+
+        foreach(GameObject userInfo in usersInfo)
+        {
+            userInfo.SetActive(false);
+        }
+
+        if(userNames.Count > 0 && userNames.Count == userResults.Count)
+        {
+            for (int i = 0; i < userNames.Count; i++)
+            {
+                usersInfo[i].SetActive(true);
+                string userInfoName = "Name" + i;
+                GameObject.Find(userInfoName).GetComponent<TextMeshProUGUI>().text = userNames[i];
+            }
+        }
+    }
+
+    public void onLoadResultsScreen()
+    {
+        List<string> top = GameManager.Instance.GetTopResults();
+        int i = 0;
+        foreach(GameObject topObj in topInfo)
+        {
+            if(i <= top.Count - 1)
+            {
+                topObj.GetComponent<TextMeshProUGUI>().text = top[i];
+            }
+            else
+            {
+                topObj.GetComponent<TextMeshProUGUI>().text = "";
+            }
+        }
+    }
+
+    public void AddNewUser()
+    {
+        string userName = GameObject.Find("NewUserNameText").GetComponent<TextMeshProUGUI>().text;
+        GameObject.Find("NewUserNameText").GetComponent<TextMeshProUGUI>().text = "";
+        GameManager.Instance.AddNewUser(userName);
+        onLoadUsersScreen();
+    }
+
+    public void DeleteUser(int id)
+    {
+        GameManager.Instance.DeleteUser(id);
+        onLoadUsersScreen();
+    }
+
+    public void SelectUser(int id)
+    {
+        GameManager.Instance.SetActiveUser(id);
+        onLoadUsersScreen();
+    }
+}
